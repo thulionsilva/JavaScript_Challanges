86eca4a283c0e407be77ac9e8dd02a30981be616
Thulio <thulionascimento1@gmail.com>
2025-03-03 14:05:14 -0300

    site simples com display de catalogo de produtos

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,29 +1,68 @@
-import logo from './capa.jpg';
+//import logo from './produto1.jpg';
 import './App.css';
-import PDF from './/documents//Bogdan M. Wilamowski, J. David Irwin - Industrial Communication Systems.pdf';
+import produtos from './produtos.json';
+//import PDF from './/documents//Bogdan M. Wilamowski, J. David Irwin - Industrial Communication Systems.pdf';
 
-function Teste() {
+//https://api.whatsapp.com/send/?phone=5581997573181&text=urlencodedtext
+
+
+
+function Cabecalho() {
   return (
+
     <div className="Teste">
-      <h2>My first Function Based Component</h2>
+      
+      <h2>Nosso catálogo</h2>
     </div>
   );
 }
+
+/*
+function Book(props) {
+  return (
+    <div className="Book">
+    <a href={props.Path} target="_blank" rel="noopener noreferrer">
+    <h4>Industrial Communication Systems</h4>
+    <img src={props.logo} className="cover" alt="logo"/>
+  </a>
+  </div>
+  );
+}
+
+*/
+function Produto(props) {
+  const numero = 5581996563134
+  const texto = "Olá! Gostei do produto " + props.dados.Nome + " ID: " + props.dados.ID + " e quero comprar. Me envie o link para pagamento."
+  const url = `https://api.whatsapp.com/send/?phone=${numero}&text=${texto}`
+  return (
+    <div className="Book">
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + props.dados.Imagem} className="cover" alt="logo"/>
+      </a>
+      <h6>{props.dados.Nome}</h6>
+      <p className="descricao">{props.dados.Descrição}</p>
+      <h6 className="preco">R${props.dados.Preço}</h6>
+    </div>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <headers className="App-header">
-        <div> year </div>
-        <div>
-          <a href={PDF} target="_blank" rel="noopener noreferrer">
-          <h4>Industrial Communication Systems</h4>
-          <img src={logo} className="cover" alt="logo"/>
-        </a>
-        </div>
         <p>
-          <Teste />
+          <Cabecalho />
         </p>
+        <div className="produtos"> 
+
+          { produtos.map((produto) => (
+            <Produto dados={produto} />
+          ))}
+          
+        </div>
+
         </headers>
 
     </div>
