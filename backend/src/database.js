1ef26e5b6c512c6eb73456d7c5784aa0826c62ac
Thulio <thulionascimento1@gmail.com>
2025-03-18 00:15:12 -0300

    backend/frontend para atualizar quantidade de itens no carrinho

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,9 +2,9 @@ const sqlite = require('sqlite3').verbose();
 const db = new sqlite.Database('carrinho.db');
 
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS compras (id INTEGER PRIMARY KEY AUTOINCREMENT, user_name TEXT, product_id INTEGER, quantity INTEGER)");
+    db.run("CREATE TABLE IF NOT EXISTS compras (id INTEGER PRIMARY KEY AUTOINCREMENT, user_name TEXT, product_id INTEGER)");
 });
 
-db.run("INSERT INTO compras (user_name, product_id, quantity) VALUES (?, ?, ?)", ["John Doe", 1, 2]);
+db.run("INSERT INTO compras (user_name, product_id) VALUES (?, ?)", ["John Doe", 1]);
 
 module.exports = db;
--- react_app/src/components/ItemCarrinho.jsx
+++ react_app/src/components/ItemCarrinho.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import produtos from '../produtos.json';
-function ItemCarrinho({product_id, quantity}) {
+function ItemCarrinho({json, page_refresh}) {
     const [product, setProduct] = useState(null);
-    
+
     useEffect(() => {
-        const product2 = produtos.find(p => p.ID === parseInt(product_id));
+        const product2 = produtos.find(p => p.ID === parseInt(json.product_id));
         
         if (product2) {
             setProduct(product2);
@@ -12,18 +12,65 @@ function ItemCarrinho({product_id, quantity}) {
         else {
             setProduct("Produto não encontrado");
         }
-    }, [product_id]);
+    }, [json.product_id]);
 
+    const removeItem = () => {
+        const response = fetch('http://localhost:3030/api/compras', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user_name: json.user_name, product_id: json.product_id, last_added: null}),
+        }); 
+        console.log(response);
+        page_refresh();
+    };
+
+    const addItem = () => {
+        const response = fetch('http://localhost:3030/api/compras', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user_name: json.user_name, product_id: json.product_id}),
+        }); 
+        console.log(response);
+        page_refresh();
+    }
+
+    const removeOneItem = () => {
+        const response = fetch('http://localhost:3030/api/compras', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user_name: json.user_name, product_id: json.product_id, last_added: json.last_added}),
+        }); 
+        console.log(response);
+        page_refresh();
+    }
 
     return (
-        <div className='item-carrinho'>
-            
+        <div>
+
             {product ? (
-                <div>
-                    <p>{quantity}</p>
+                <div className='item-carrinho'>
+                    <img src={product.Imagem} alt={product.Nome} style={{height: '100%'}} />
+                    <div style={{width: '200px', textAlign: 'center', fontSize: '15px'}}>
                     <p>{product.Nome}</p>
+                    </div >
+
+                    <div style={{width: '20px', textAlign: 'center', fontSize: '20px'}}>
                     <p>{product.Preço}</p>
-                    <p>{product.Preço * quantity}</p>
+                    </div>
+
+                    <div style={{display: 'flex',flexDirection: 'row' ,width: '15px', textAlign: 'center', fontSize: '20px', alignItems: 'center', gap: '5px'}}>
+                    <button onClick={removeOneItem} style={{height: '20px', textAlign: 'center', fontWeight: 'bold'}}>-</button>
+                    <p>{json.quantity}</p>
+                    <button onClick={addItem} style={{height: '20px', textAlign: 'center', fontWeight: 'bold'}}>+</button>
+                    </div>
+
+                    <button onClick={removeItem} >Remover</button>
                 </div>
             ) : (
                 <p>Loading...</p>
